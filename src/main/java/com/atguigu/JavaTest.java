9b1715ebaf936a16a38086339e81119439f2271a
lyh007-dot <67141627+lyh007-dot@users.noreply.github.com>
2022-07-17 18:27:44 +0800

    Update JavaTest.java

--- src/main/java/com/atguigu/JavaTest.java
+++ src/main/java/com/atguigu/JavaTest.java
@@ -9,6 +9,6 @@ public class JavaTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
-        System.out.println("push test");
+        System.out.println("pull test");
     }
 }
